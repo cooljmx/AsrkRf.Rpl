0f453fc48f7cd68d276deeb3ffd93b38c2ed25f4
cooljmx <cooljmx@gmail.com>
2014-10-24 14:53:10 +0400

    Скрипты и конфиги

---
+++ Scripts/Firebird.Subdivision.Get.js
@@ -0,0 +1,15 @@
+var equalData = function (input, output) {
+    output.IdSubdivision = input.IdPodrazdelenie;
+    output.SubdivisionName = input.Podrazdname;
+    output.SubdivisionShortName = input.PodrazdnameSokr;
+    output.IdRegion = input.IdRegion;
+}
+
+podrazdelenieList.forEach(function (element) {
+    equalData(element, cloud);
+    if (element.IdParent != 0) {
+        cloud.Parent = cloud.CreateParent();
+        cloud = cloud.Parent;
+    }
+});
+
