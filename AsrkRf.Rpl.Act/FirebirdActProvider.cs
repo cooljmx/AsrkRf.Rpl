32fc7141fd90b033dd1d2f62acc2860e72e38835
cooljmx <cooljmx@gmail.com>
2014-10-28 16:50:29 +0300

    Контроллер и провайдер (пустой) для акта

---
+++ AsrkRf.Rpl.Act/FirebirdActProvider.cs
@@ -0,0 +1,26 @@
+﻿using AsrkRf.Rpl.Common;
+using AsrkRf.Rpl.Session;
+
+namespace AsrkRf.Rpl.Act
+{
+    public class FirebirdActProvider : IFirebird
+    {
+        private readonly ISessionHelper sessionHelper;
+        private readonly IUriHelper uriHelper;
+
+        public FirebirdActProvider(ISessionHelper sessionHelper, IUriHelper uriHelper)
+        {
+            this.sessionHelper = sessionHelper;
+            this.uriHelper = uriHelper;
+        }
+
+        public object Get(decimal id)
+        {
+            return null;
+        }
+
+        public void Post(string value)
+        {
+        }
+    }
+}
---
+++ AsrkRf.Rpl.WebServer/Controllers/ActController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using AsrkRf.Rpl.Act;
+using AsrkRf.Rpl.Common;
+using AsrkRf.Rpl.Session;
+
+namespace AsrkRf.Rpl.WebServer.Controllers
+{
+    public class ActController : ApiController
+    {
+        private readonly FirebirdActProvider provider;
+
+        public ActController(ISessionHelper sessionHelper, IUriHelper uriHelper)
+        {
+            provider = new FirebirdActProvider(sessionHelper, uriHelper);
+        }
+
+        public object Get(long id)
+        {
+            return provider.Get(id);
+        }
+
+        public void Post([FromBody]string value)
+        {
+            provider.Post(value);
+        }
+    }
+}
